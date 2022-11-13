2111408d6e5824cf8f6e1a5ab790289327040558
Muhammad <nurmirzayevmuhammad6@gmail.com>
2022-11-13 10:18:13 +0500

    extra exercise

---
+++ index.js
@@ -0,0 +1,116 @@
+// ============
+// let a = 3;
+// let b = 12;
+// console.log(a > b);
+
+// =============
+// let a = 20;
+// let b = "22";
+// console.log(a + b); 
+
+// =============
+// let a = 20;
+// let b = "22";
+// console.log(a + +b);
+
+// =============
+// let str1 = "Nurmirzayev"
+// let str2 = " Muhammad" 
+// console.log(str1 + str2);
+
+// =============
+// let a = 7;
+// let b = 3;
+// console.log(a - b);
+
+// =============
+
+// let str1="Webbrain";
+// let str2="Academy";
+// console.log(str1 - str2);
+
+// =============
+// let a = 22;
+// let b = 3.3;
+// console.log("Checking a:");
+// console.log(Number (a));
+// console.log(Number.parseInt (a));
+// console.log(Number.parseFloat(a))
+
+// console.log("Checking b:");
+// console.log(Number (b));
+// console.log(parseInt (b));
+// console.log(parseFloat(b))
+
+// ==============
+// console.log(false == 0);
+// console.log(true == 1);
+// console.log(null == 0);
+// console.log(undefined == 0);
+// console.log(null == undefined);
+
+// console.log(true == "1");
+// console.log(false == "0");
+// console.log(true == "a");
+
+// ===========
+
+// let a = 3;
+// let b = 4;
+
+// console.log(a*b);
+
+//  ============
+// let a = 2;
+// let b = 1;
+// console.log(a**b);
+
+// ===========
+
+// let a = 3;
+// let b = 27;
+
+// console.log(b / a );
+
+// ===========
+
+// let a = 10;
+// let b = 3;
+
+// console.log(10 % 3);
+
+// ========
+
+// let a = 2;
+// console.log(a++);
+// console.log(++a);
+// console.log(a--) ;
+// console.log(++a);
+
+// =========
+
+// let a = 25;
+
+// a+=5;
+// console.log(a);
+
+// let b = 35;
+
+// b= b+ 15;
+// b+=10;
+// console.log(b) 
+
+
+// =========
+
+let a = 20;
+// let b = 10;
+// let c = "10";
+
+
+// console.log(a > b);        //true
+// console.log(b >= c );     //true
+// console.log(c <= b);     //true
+// console.log(c == b);    //true
+// console.log(c === b);  //false
+
