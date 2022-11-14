034d2491dcdc6fa0302ca84f1b1754f7fde33636
Muhammad <nurmirzayevmuhammad6@gmail.com>
2022-11-14 20:17:40 +0500

    new file

---
+++ 1411n/index.js
@@ -0,0 +1,29 @@
+// let a = 3;
+// let b = 4;
+
+// if(a>b){
+//     console.log(a);
+// }
+// else{
+//     console.log(b);
+// }
+
+// let a = -1;
+
+// if(a>=25){
+//     console.log("Havo issiq");
+// }
+// else if(a>=15){
+//     console.log("Havo bulitli");
+// }
+// else if(a>=10){
+//     console.log("Yomgirli ob-havo");
+// }
+// else if(a>=0){
+//     console.log("Havo juda sovuq");
+// }
+// else{
+//     console.log("qor");
+// }
+
+
