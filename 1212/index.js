fb2dc451c1e2eaf55702fb17f9ccbb9461423457
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-12-12 07:01:44 +0500

    distructure tutorial

---
+++ 1212/index.js
@@ -0,0 +1,15 @@
+// let obj = {name:"Webbrain",title:"It center",child:{year:1111}};
+
+
+// let name="wba"
+// let {name:Newname,title,child:{year:newyear}}=obj;
+
+// console.log(newyear);
+
+
+
+// let ar=["apple","banana","kiwi"];
+
+// let[bir,...res]=ar;
+// console.log(res);
+
