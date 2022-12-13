283a1568d33b7d5185c2431ce7226db8fb7574cd
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-12-13 05:02:22 +0500

    JSON and MOMENT JS

---
+++ 1312/index.js
@@ -0,0 +1,36 @@
+// // New Date
+
+// const getToday=(symbol)=>{
+//     let date=new Date();
+//     return `Today: ${date.getDate()}${symbol}${date.getMonth()+1}${symbol}${date.getFullYear()}`
+// }
+// console.log(getToday("/"));
+
+    // let date=new Date();
+    // console.log(date.getHours());
+    // console.log(date.getMinutes());
+    // console.log(date.getSeconds());
+    // console.log(date.getMilliseconds());
+
+
+    // let date=new Date();
+    // let weeks=["Yakshanba","Dushanba","Seshanba","Chorshanba","Juma","Shanba"];
+    // console.log(weeks[date.getDay()].slice(0,4));
+
+
+    //Json
+    // let obj={name:"Muhammad"};
+    // console.log(JSON.stringify(obj));
+
+
+//     let obj={name:"Muhammad"};
+// let json=(JSON.stringify(obj));
+// let user=JSON.parse(json)
+// console.log(json);
+// console.log(user);
+
+// let obj={name:"Muhammad",age:"19",child:{fullname:"Nurmirzayev Muhammad"}};
+// console.log(JSON.stringify(obj,["name","age","child","fullname"],4));
+
+
+
