a784b90f5643eddf14406c4a0075e26507b77c30
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-12-15 13:22:43 +0500

    recurtion

---
+++ 1512/index.js
@@ -0,0 +1,58 @@
+// let res=0;
+// const sum = (n) => {
+//   if(n === 9){
+//     return res+=9;
+//   }
+//   else{
+//     res+=n;
+//     return sum(n+1)
+//   }  
+  
+// }
+// sum(1);
+// console.log(res);
+
+
+
+
+// let company = {
+//   sales: [
+//     { name: "John", salary: 1000 },
+//     { name: "Alice", salary: 1600 },
+//   ],
+//   development: {
+//     sites: [
+//       { name: "Peter", salary: 2000 },
+//       { name: "Alex", salary: 1800 },
+//     ],
+//     dev: [
+//       { name: "Peter", salary: 2000 },
+//       { name: "Alex", salary: 1800 },
+//     ],
+//     internals: [{ name: "Jack", salary: 1300 }],
+//   },
+// };
+
+
+// function getCalc(company){
+//   if(Array.isArray(company)){
+//     return company.reduce((p,c)=>p+c.salary,0)
+//   }
+//   else{
+//     let sum=0;
+//     for(let s of Object.values(company)){
+//       sum+=getCalc(s);
+//     }
+//     return sum;
+//   }
+// }
+// console.log(getCalc(company));
+
+for(i=1;i<10;i++){
+  
+}
+console.log(i);
+for (i;i<10;i++){
+}
+console.log(i);
+
