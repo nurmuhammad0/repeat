769204096c42d03ecddfcd29481732e70d86cc13
nurmuhammad0 <nurmirzayevmuhammad6@gmail.com>
2022-12-26 13:17:51 +0500

    generator

--- 2612/index.js
+++ 2612/index.js
@@ -1,12 +1,70 @@
-const samplePromise=()=>Promise.resolve("A");
+// const samplePromise=()=>Promise.resolve("A");
 
-function f(){
-    samplePromise().then(result=>console.log(result));
-    console.log("B");
-} 
-async function g(){
-    console.log(await samplePromise());
-    console.log("B");
-}
-f();
-g()
+// function f(){
+//     samplePromise().then(result=>console.log(result));
+//     console.log("B");
+// } 
+// async function g(){
+//     console.log(await samplePromise());
+//     console.log("B");
+// }
+// f();
+// g()
+
+
+// function* getData(){
+//     yield 1;
+//     yield 2;
+// }
+// const g=getData();
+// const generator=getData();
+
+// console.log(g.next());
+// console.log(g.next());
+// console.log(generator.next());
+
+// //array;
+// console.log([...g]);
+
+
+// for(let i of g){
+//     console.log(i);
+// }
+
+
+// console.log(g.next());
+// console.log(g.next());
+// console.log(g.next());
+
+// function* getData(){
+//     let id=user.length;
+//     while(true){
+//         yield ++id;
+//     }
+// }
+// const g=getData();
+
+
+// let user=[
+// {id:1,name:"user1"},
+// {id:2,name:"user2"},
+// {id:3,name:"user3"},
+// {id:4,name:"user4"},
+// {id:5,name:"user5"},
+// {id:6,name:"user6"},
+// ];
+// const onDelete=(id)=>{
+// let res=user.filter((v)=>v.id !==id);
+// user=res;
+// }
+
+
+// const add =()=>{
+// user=[...user,{id:g.next().value,name:"user7"}]
+// }
+// add();
+// onDelete(3);
+// add();
+// add();
+// add();
+// console.log(user);
